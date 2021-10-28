b72a5ba9ca918659456dcf015285c92e66138039
maximus7022 <pash6790@gmail.com>
2021-10-28 21:25:36 +0300

    solving mistake in word

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 print("Sosochek pidarasa")
 cock_size = input("Enter your cock size: ")
 if int(cock_size) > 10:
-    print("WOW! Nce cock!)")
+    print("WOW! Nice cock!)")
 else:
     print("You are cockumber")
