2b5dc8a97890caccfce40574f99f2f95259f64c2
maximus7022 <pash6790@gmail.com>
2021-10-30 16:23:11 +0300

    adding text filtering/all following files

---
+++ script.py
@@ -0,0 +1,54 @@
+
+
+#\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/МАСИВ ЛІТЕР\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/#
+
+chars = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+         'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ы', 'ь', 'э', 'ю', 'я']
+
+#\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/ФІЛЬТРАЦІЯ ТЕКСТУ\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/#
+
+
+def Char_filter(text):       # функція фільтрації без пробілів
+    A = []
+    text = text.lower()     # перевід в нижній регістр
+
+    for ch in text:     # фільтрація зайвих символів
+        if ch in chars:
+            A.append(ch)
+
+    text = ''.join(A)
+    text = text.replace('ъ', 'ь')   # заміна 'ъ' на 'ь'
+    text = text.replace('ё', 'е')   # заміна 'ё' на 'е'
+
+    return text
+
+
+def Char_with_space_filter(text):       # функція фільтрації з пробілами
+    A = []
+    text = text.lower()     # перевід в нижній регістр
+
+    for ch in text:     # фільтрація зайвих символів
+        if ch in chars or ch == ' ':
+            A.append(ch)
+
+    text = ''.join(A)
+    text = text.split()     # фільтрація зайвих пробілів
+    text = ' '.join(text)
+    text = text.replace('ъ', 'ь')   # заміна 'ъ' на 'ь'
+    text = text.replace('ё', 'е')   # заміна 'ё' на 'е'
+
+    return text
+
+
+# відкриття файла з текстом і його зчитування
+f = open(".\\duna.txt", "r", encoding='UTF-8')
+duna_text = f.read()
+f.close()
+
+f = open(".\\Filtered txt\\filtered.txt", "w")
+f.write(Char_filter(duna_text))
+f.close()
+
+f = open(".\\Filtered txt\\filtered_with_space.txt", "w")
+f.write(Char_with_space_filter(duna_text))
+f.close()
