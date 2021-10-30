e9856f30d74653c06d068f1dcafe31ff3bbd3d96
maximus7022 <pash6790@gmail.com>
2021-10-30 19:16:25 +0300

    adding monograms/following files

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-
+import os       # для видалення файлів
 
 #\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/МАСИВ ЛІТЕР\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/#
 
@@ -47,10 +47,43 @@ f.close()
 
 # відкриття/створення файла і запис в нього відфільтрованого тексту
 f = open(".\\Filtered txt\\filtered.txt", "w")
-f.write(Char_filter(duna_text))
+text = Char_filter(duna_text)
+f.write(text)
 f.close()
 
 # відкриття/створення файла і запис в нього відфільтрованого тексту з пробілами
 f = open(".\\Filtered txt\\filtered_with_space.txt", "w")
-f.write(Char_with_space_filter(duna_text))
+text_with_space = Char_with_space_filter(duna_text)
+f.write(text_with_space)
+f.close()
+
+#\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/МОНОГРАМИ\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/#
+
+
+def Monogram_freq(text, i):         # функція підрахунку частот монограм
+    if ' ' in text and i == 0:      # перевірка чи є пробіли в тексті
+        chars.append(' ')
+    freq = text.count(chars[i]) / len(text)     # підрахунок
+    res_str = chars[i] + "  -->  " + str(freq) + "\n"
+    return res_str
+
+
+# перевірка чи існує вже файл щоб при повторному виконанні програми файл перезаписувався заново
+if os.path.exists(".\\Mono&Bigrams\\monograms.txt"):
+    os.remove(".\\Mono&Bigrams\\monograms.txt")
+
+# запис монограм без пробілу і їх частот в файл
+f = open(".\\Mono&Bigrams\\monograms.txt", "a")
+for i in range(0, len(chars)):
+    f.write(Monogram_freq(text, i))
+f.close()
+
+# перевірка чи існує вже файл щоб при повторному виконанні програми файл перезаписувався заново
+if os.path.exists(".\\Mono&Bigrams\\monograms_with_space.txt"):
+    os.remove(".\\Mono&Bigrams\\monograms_with_space.txt")
+
+# запис монограм з пробілом і їх частот в файл
+f = open(".\\Mono&Bigrams\\monograms_with_space.txt", "a")
+for i in range(0, len(chars)+1):
+    f.write(Monogram_freq(text_with_space, i))
 f.close()
